a6d503a7b538991741fab50558df77407cc7191f
Velasco <92410903+Vex4444@users.noreply.github.com>
2022-08-17 11:13:49 -0500

    Igualacion de Funciones de Transferencias
    Ultimo paso de etapa 3, falta igualar terminos iguales en el denominador de Td y T
    
    Hace falta despejar manualmente o a través de matlab wn y zeta, en vez de simplemente escrito como en el codigo ver comments

--- etapa1.m
+++ etapa1.m
@@ -12,11 +12,26 @@ Ts=1.2; %Tiempo de asentamiento (sec)
 syms s
 Gp=simplify(K/((J*s+b)*(L*s+R)+K^2)) %transferencia de motor
 
+%Zeta y wn hay que despejarlas de Ts y OT en PPT
+
 zeta=sqrt((log(OS))^2/((log(OS))^2+pi^2)) %amortiguamiento deseado
 wn=4/(zeta*Ts) %frecuencia natural deseada
 
 Td=simplify(wn^2/(s^2+2*zeta*s+wn^2))    %transferencia deseada
 
 syms ti td Kc C %ti td Kc son coeficientes de la funcion de transferencia del controlador
-Gc=Kc*(1+1/ti*s+td*s) %Transferencia de controlador
+Gc=Kc*(1+1/(ti*s)+td*s) %Transferencia de controlador
 H=C/C; %Parece redundante, es el sensor, C es la salida, sirve a la hora de hacerlo iterativo
+
+T=simplify(Gc*Gp/(1+H*Gc*Gp))
+
+polos=poles(Td); %polos FT deseada
+polo1=double(polos(1)) %Polo1
+polo2=double(polos(2)) %Polo2
+a=10*real(polo1);%Polo agregado para igualar Td a T
+
+Td=Td/(s+a); %TD con polo agregado
+eq1=Td==T
+pretty(eq1) %Hay que multiplicar T*s/s para tenerlo en terminos de s^3+s^2+s+A
+
+%Igualando terminos de los denominadores se obtienen ti td y Kc 
