221ebcb661930b0f4842cf770604a6d04387e05d
G00Z_G00Z <edygomezgg4@gmail.com>
2022-08-17 09:35:37 -0500

    Etapa 1: Damping factor, K, wn, rlocus

--- etapa1.m
+++ etapa1.m
@@ -1,11 +1,21 @@
-clc
 clear
+clc
 J=3.78E-3;
 R=13.6;
 K=0.044;
 L=10.51E-3;
 b=4E-3;
 
-syms s
-G=K/((J*s+b)*(L*s+R)+K^2)
-simplify(G)
+s = tf('s');
+P_motor = K/((J*s+b)*(L*s+R)+K^2)
+
+P_motor / 3.973e-05
+
+wn = sqrt(2.238e-6)
+
+K = 0.044 / wn^2
+
+zeta = 0.05145 / (2 * wn)
+
+grid on 
+rlocus(P_motor)
