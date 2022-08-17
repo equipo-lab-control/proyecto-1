23b4f0e2e66d0cab4bab3e4b9f3223c7667c7cdd
Velasco <92410903+Vex4444@users.noreply.github.com>
2022-08-17 09:43:09 -0500

    funcion de transferencia de controlador
    Adelanto de etapa 3, paso antes de conseguir los parametros del PID

--- etapa1.m
+++ etapa1.m
@@ -1,5 +1,6 @@
 clc
 clear
+%datos motor
 J=3.78E-3;
 R=13.6;
 K=0.044;
@@ -7,13 +8,15 @@ L=10.51E-3;
 b=4E-3;
 
 OS=0.15; %porcentaje sobre impulso (no es porcentaje)
-Ts=1.2; %Tiempo de asentamiento (s)
+Ts=1.2; %Tiempo de asentamiento (sec)
 syms s
-Gp=simplify(K/((J*s+b)*(L*s+R)+K^2))
+Gp=simplify(K/((J*s+b)*(L*s+R)+K^2)) %transferencia de motor
 
-A=sqrt((log(OS))^2/((log(OS))^2+pi^2))
-wn=4/(A*Ts)
+zeta=sqrt((log(OS))^2/((log(OS))^2+pi^2)) %amortiguamiento deseado
+wn=4/(zeta*Ts) %frecuencia natural deseada
 
-Td=simplify(wn^2/(s^2+2*A*s+wn^2))    %transferencia deseada
+Td=simplify(wn^2/(s^2+2*zeta*s+wn^2))    %transferencia deseada
 
-%Gc=K*(1+1/)
+syms ti td Kc C %ti td Kc son coeficientes de la funcion de transferencia del controlador
+Gc=Kc*(1+1/ti*s+td*s) %Transferencia de controlador
+H=C/C; %Parece redundante, es el sensor, C es la salida, sirve a la hora de hacerlo iterativo
