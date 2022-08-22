9fd98c866188108292d1701352dd12332bcbd0f3
G00Z_G00Z <edygomezgg4@gmail.com>
2022-08-22 11:59:22 -0500

    Uncommented everything

--- src/etapa_2.m
+++ src/etapa_2.m
@@ -3,39 +3,39 @@ clc
 
 load('data/Ke.mat')
 
-%clc
-%J=3.78E-3;
-%R=13.6;
-%K=0.044;
-%L=10.51E-3;
-%b=4E-3;
+% todo: exportar los datos iniciales
+J=3.78E-3;
+R=13.6;
+K=0.044;
+L=10.51E-3;
+b=4E-3;
 
-%s = tf('s');
+s = tf('s');
 
-%P_motor = K/((J*s+b)*(L*s+R)+K^2)
+P_motor = K/((J*s+b)*(L*s+R)+K^2)
 
-%P_motor_simplify = P_motor / 3.973e-05
+P_motor_simplify = P_motor / 3.973e-05
 
-%wn = sqrt(2.238e-6)
+wn = sqrt(2.238e-6)
 
-%K = 0.044 / wn^2
+K = 0.044 / wn^2
 
-%zeta = 0.05145 / (2 * wn)
+zeta = 0.05145 / (2 * wn)
 
 
-%grid on 
-%%rlocus(P_motor)
-%hold off
+grid on 
+%rlocus(P_motor)
+hold off
 
-%% Etapa 2
+% Etapa 2
 
-%polos = pole(P_motor)
-%zero = zero(P_motor)
+polos = pole(P_motor)
+zero = zero(P_motor)
 
-%grid on 
-%hold on 
-%bode(P_motor)
+grid on 
+hold on 
+bode(P_motor)
 
-%hold off
+hold off
 
 
