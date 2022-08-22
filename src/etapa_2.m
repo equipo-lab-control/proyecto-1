f5aa779bb608b212cf2fc348d452f8261d6b1862
G00Z_G00Z <edygomezgg4@gmail.com>
2022-08-22 11:53:02 -0500

    Imported data/Ke.mat

---
+++ src/etapa_2.m
@@ -0,0 +1,41 @@
+clear 
+clc
+
+load('data/Ke.mat')
+
+%clc
+%J=3.78E-3;
+%R=13.6;
+%K=0.044;
+%L=10.51E-3;
+%b=4E-3;
+
+%s = tf('s');
+
+%P_motor = K/((J*s+b)*(L*s+R)+K^2)
+
+%P_motor_simplify = P_motor / 3.973e-05
+
+%wn = sqrt(2.238e-6)
+
+%K = 0.044 / wn^2
+
+%zeta = 0.05145 / (2 * wn)
+
+
+%grid on 
+%%rlocus(P_motor)
+%hold off
+
+%% Etapa 2
+
+%polos = pole(P_motor)
+%zero = zero(P_motor)
+
+%grid on 
+%hold on 
+%bode(P_motor)
+
+%hold off
+
+
