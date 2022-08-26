e0630f2e23778c4e48e7c386c023ca08044e10a3
G00Z_G00Z <edygomezgg4@gmail.com>
2022-08-26 08:41:00 -0500

    Moved some variables to the side

--- etapa_1.m
+++ etapa_1.m
@@ -10,21 +10,22 @@ L=10.51E-3;
 b=4E-3;
 
 table_motor_2 = readtable('./data/motor_2.csv' );
-table_motor_2.Properties.VariableNames = { 'vel_rads', 'vel_rpm', 'volts' }
+table_motor_2.Properties.VariableNames = { 'vel_rads', 'vel_rpm', 'volts' };
 
 table_vel_rads_volts = removevars(table_motor_2,"vel_rpm");
 
-
 model = fitlm(table_vel_rads_volts);
 
-Ke = model.Coefficients{"vel_rads","Estimate"}
+Ke = model.Coefficients{"vel_rads","Estimate"};
 
-
-% Saves the variable Ke in Ke file
-save('data/Ke', 'Ke')
-save('data/initial', 'J', 'R', 'L', 'b')
 
 % todo: poner la funcion para guardar figura
 % plot(model)
+
+% Saving data
+save('data/Ke', 'Ke');
+save('data/initial', 'J', 'R', 'L', 'b');
+save('data/table_motors', 'table_vel_rads_volts');
+
 
 
